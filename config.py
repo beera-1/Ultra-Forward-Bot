b8e396fc2831fe292f9e83ba8073d786a93f30fb
beera-1 <217328278+beera-1@users.noreply.github.com>
2025-09-16 14:23:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "09e3e8468c536526c22e5d644de244ab")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://forward:forward@cluster0.neua9ax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "nanduguru")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6390511215').split()]
 
